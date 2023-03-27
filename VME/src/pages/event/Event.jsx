c566b2cb12a6a7a4a609a0ff8493cc5d542e5a8f
Sanush Shakya <shakyasanush7@gmail.com>
2023-03-27 08:31:35 +0545

    Project Set Up

---
+++ VME/src/pages/event/Event.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Event.scss"
+
+const Event = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Event
---
+++ VME/src/pages/events/Events.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Events.scss"
+
+const Events = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Events
